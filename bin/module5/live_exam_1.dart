4861549954223556a05064396eb57347739d5a77
Mahin <71384705+rimahin@users.noreply.github.com>
2025-06-26 00:00:23 +0600

    live_exam_1.dart

--- bin/module5/live_exam_1.dart
+++ bin/module5/live_exam_1.dart
@@ -1,6 +1,6 @@
 main(){
   List<Map<String, dynamic>> students = [
-    {'name' : 'Alice', 'scores' : [85, 90, 78]},
+    {'name' : "Alice", "scores" : [85, 90, 78]},
     {"name": "Bob", "scores": [88, 76, 95]},
     {"name": "Charlie", "scores": [90, 92, 85]}
   ];
